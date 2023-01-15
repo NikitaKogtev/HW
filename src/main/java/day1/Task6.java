2e70b82fbc1658738a6db7c58719eb89615b1d0c
Nikita Kogtev <nikitakogtev@gmail.com>
2023-01-15 20:37:21 +0700

    Day1.1

--- src/main/java/day1/Task6.java
+++ src/main/java/day1/Task6.java
@@ -3,8 +3,8 @@ package day1;
 public class Task6 {
     public static void main(String[] args) {
         int k = 4;
-        for (int i = 1; i < 10; i++){
-            System.out.println(i + " x " + k + " = " + i*k);
+        for (int i = 1; i < 10; i++) {
+            System.out.println(i + " x " + k + " = " + i * k);
         }
     }
 }
