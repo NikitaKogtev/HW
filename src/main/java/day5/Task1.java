b5c263a57cdfeca7ac46a530c352cbb5ac45c26b
Nikita Kogtev <nikitakogtev@gmail.com>
2023-01-16 21:11:16 +0700

    Day5

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -2,6 +2,13 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
+        Car car = new Car();
 
+        car.setModel("Hyundai");
+        car.setColor("White");
+        car.setYear(2020);
+
+        System.out.println("Car model: " + car.getModel() + " , " + "Car color: " + car.getColor() + " , " +
+                "Car year: " + car.getYear());
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -4,5 +4,9 @@ package day5;
 public class Task2 {
     public static void main(String[] args) {
 
+        Motorbike motorbike = new Motorbike("Yamaha", "White", 2015);
+
+        System.out.println("Bike model: " + motorbike.getModel() + " , " + "Bike color: " + motorbike.getColor() + " , " +
+                "Bike year: " + motorbike.getYear());
     }
 }
