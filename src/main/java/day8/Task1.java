e124754cc739a8fa6711b41edc770f0512433bf6
Nikita Kogtev <nikitakogtev@gmail.com>
2023-01-20 21:55:40 +0700

    complete day8 tasks

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -3,5 +3,26 @@ package day8;
 public class Task1 {
     public static void main(String[] args) {
 
+        long startTime = System.currentTimeMillis();
+        String s = "";
+
+        for (int i = 0; i <= 20_000; i++) {
+            s += i + " ";
+        }
+        //       System.out.println(s);
+        long stopTime = System.currentTimeMillis();
+
+        System.out.println("Время работы программы 1: " + (stopTime - startTime));
+
+        long startTime2 = System.currentTimeMillis();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i <= 20_000; i++) {
+            stringBuilder.append(i);
+            stringBuilder.append(" ");
+        }
+        //       System.out.println(stringBuilder.toString());
+        long stopTime2 = System.currentTimeMillis();
+
+        System.out.println("Время работы программы 2: " + (stopTime2 - startTime2));
     }
 }
