1aa8d5ff54219230cc37af5c6f47b2563d118846
Nikita Kogtev <nikitakogtev@gmail.com>
2023-02-10 20:58:52 +0700

    complete day19 days tasks

--- src/main/java/day19/Task1.java
+++ src/main/java/day19/Task1.java
@@ -1,10 +1,58 @@
 package day19;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
 /**
  * @author Neil Alishev
  */
 public class Task1 {
     public static void main(String[] args) {
+        File file = new File("C:\\Users\\NikitaKogtev\\IdeaProjects\\" +
+                "JavaMarathon2021\\src\\main\\resources\\dushi.txt");
 
+        HashMap<String, Integer> map = new HashMap<>();
+
+        try {
+            Scanner scanner = new Scanner(file);
+            scanner.useDelimiter("[.,:;()?!\"\\s–]+");
+
+            List<String> list = new ArrayList<>();
+
+            while (scanner.hasNextLine()) {
+                String[] text = scanner.nextLine().split(" ");
+                list.addAll(Arrays.asList(text));
+            }
+
+            for (String s : list) {
+                if (map.containsKey(s)) {
+                    map.put(s, map.get(s) + 1);
+                } else {
+                    map.put(s, 1);
+                }
+            }
+
+            List<Integer> top = new ArrayList<>(map.values());
+            Collections.sort(top);
+            Collections.reverse(top);
+
+            List<String> topWords = new ArrayList<>();
+
+            for (int i = 0; i < 100; i++) {
+                for (Map.Entry<String, Integer> entry : map.entrySet()) {
+                    if (entry.getValue() == top.get(i)) {
+                        topWords.add(entry.getKey());
+                    }
+                }
+            }
+
+            System.out.println(topWords);
+
+            System.out.println(map.get("Чичиков")); // Чичиков - 315
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        }
     }
 }
--- src/main/java/day19/task2/Task2.java
+++ src/main/java/day19/task2/Task2.java
@@ -1,10 +1,58 @@
 package day19.task2;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
 /**
  * @author Neil Alishev
  */
 public class Task2 {
     public static void main(String[] args) {
+        HashMap<Integer, Point> map = new HashMap<>();
+
+        File file = new File("C:\\Users\\NikitaKogtev\\IdeaProjects\\" +
+                "JavaMarathon2021\\src\\main\\resources\\taxi_cars.txt");
+
+        try {
+            Scanner scanner = new Scanner(file);
+
+            while (scanner.hasNextLine()) {
+                String[] str = scanner.nextLine().split(" ");
+                map.put(Integer.parseInt(str[0]), new Point(Integer.parseInt(str[1]), Integer.parseInt(str[2])));
+
+            }
+
+            Scanner scanner1 = new Scanner(System.in);
+            String line = scanner1.nextLine();
+            String[] str = line.split(" ");
+
+            int[] num = new int[str.length];
+            int counter = 0;
+            for (String s : str) {
+                num[counter++] = Integer.parseInt(s);
+            }
+
+            Point firstSquare = new Point(num[0], num[1]);
+            Point secondSquare = new Point(num[2], num[3]);
+
+            List<Integer> listCar = new ArrayList<>();
+
+            for (Map.Entry<Integer, Point> entry : map.entrySet()) {
+                if (entry.getValue().getX() > firstSquare.getX() && entry.getValue().getX() < secondSquare.getX()
+                        && entry.getValue().getY() > secondSquare.getY() && entry.getValue().getY() < firstSquare.getY()) {
+
+                    listCar.add(entry.getKey());
+                }
+            }
+
+            System.out.println(listCar);
+
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        }
+
 
     }
 }
